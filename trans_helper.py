890dbb3dd3e02c32811cf91d3f4c1ac0b670cff4
Eli <benevedeselias@gmail.com>
2019-04-27 15:13:27 -0400

    Helper updated

--- trans_helper.py
+++ trans_helper.py
@@ -28,6 +28,43 @@ def main():
         rows = int(sys.argv[1])
         cols = int(sys.argv[2])
 
+    A = []
+    B = []
+
+    for r in range(32):
+        A.append([])
+        B.append([])
+        for c in range(32):
+            A[-1].append(c + 32*r)
+            B[-1].append(0)
+
+    for r in range(32):
+        for c in range(32):
+            print("{:4d}".format(A[r][c]), end = " ")
+            if((c + 1) % 8 == 0):
+                print("  ", end = " ")
+        print("")
+        if((r + 1) % 8 == 0):
+            print("")
+
+    print("")
+    print("")
+
+    for block_row in range(4):
+        for block_col in range(4):
+            for row in range(8):
+                for col in range(8):
+                    B[8*block_col + col][8*block_row + row] = A[8*block_row + row][8*block_col + col]
+
+    for r in range(32):
+        for c in range(32):
+            print("{:4d}".format(B[r][c]), end = " ")
+            if((c + 1) % 8 == 0):
+                print("  ", end = " ")
+        print("")
+        if((r + 1) % 8 == 0):
+            print("")
+
     for r in range(rows):
         for c in range(cols):
             print("{:2d}".format(get_set_and_tag(c*4 + (cols*4)*r, 54, 5)[1]), end = " ")
@@ -48,6 +85,8 @@ def main():
         print("")
         if((r + 1) % 8 == 0):
             print("")
+
+
 
     '''while True:
         r = int(input("Row: "), 16)
